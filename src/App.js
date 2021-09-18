0e90a68c9b75b3bd14becd17807657b603c89386
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-18 15:08:34 +0200

    add yassine task to app js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yassine task 😎
         </a>
       </header>
     </div>
